adae7e371b54a566ce120b1eed35a74c9a9d2037
Yatin Ajmani <yatinajmani@github.com>
2019-02-19 15:06:50 +0530

    First Exercise.

---
+++ src/com/ttn/assignment/Exerciser1.java
@@ -0,0 +1,11 @@
+package com.ttn.assignment;
+
+public class Exerciser1 {
+
+    public static void main(String[] args) {
+        String str = "string";
+        System.out.println("Original String = " + str);
+        str = str.replace("ri", "i");
+        System.out.println("Modified String = " + str);
+    }
+}
